03d801e80f99d5252454998cfa6363f2c1ca152c
SanjayChetlure <sanjay.chetlure@gmail.com>
2025-05-09 08:53:00 +0530

    9th May 2025 Multibrowser Testing

--- TestClasses/conftest.py
+++ TestClasses/conftest.py
@@ -1,7 +1,27 @@
+from email.policy import default
+
 import pytest
 from  selenium import  webdriver
 
+#step3: modify fixture for multiple browser
 @pytest.fixture
-def openbrowser():
-    driver=webdriver.Chrome()
-    return driver
+def openbrowser(browser):
+    if browser=="chrome":
+        driver=webdriver.Chrome()
+        return driver
+    elif browser=="edge":
+        driver=webdriver.Edge()
+        return driver
+    elif browser=="firefox":
+        driver=webdriver.Firefox()
+        return driver
+
+#Step2: use to get browser name from command - line & pass value browser to openbrowser fixture
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+#step1: set default value of browser
+def pytest_addoption(parser):
+    parser.addoption("--browser",action="store",default="chrome",
+                     help="provide browser name - chrome, firefox, edge, etc")
